c79e04316bce17fe485864b9fb76d7d8e399660a
JLuc <jluc@no-log.org>
2020-06-02 23:24:16 +0200

    first working version
    looks nice

--- autotypo.py
+++ autotypo.py
@@ -1,35 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File: autotypo.py - fix a text typography according to french typo rules
- for original autoquote file
-# © 2020.06 Creation of autotypo by JLuc 
-# Following © 2013 enhancements by JLuc of © 2010 autoquote.py by Gregory Pittman
+# File: autotypo.py - fix a text typography according to french typo rules for original autoquote file
+# © 2020.06 Creation of autotypo by JLuc following © 2013 enhancements by JLuc of © 2010 autoquote.py by Gregory Pittman
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-#
-"""
-LIMITS
-- it only acts on the currently selected frame 
-(todo : acting on more than one selected frame,
-or on whole chain of linked text frame would be nice).
-- TODO : Spaces are not added outside of the quotes.
-(that could be a job for another more general typographic tool)
 
-INSTALL
-download file, save it with 'autotypo.py' name
-and enjoy
-
-USAGE
-You must have a document open, and a text frame selected.
-Run script from scribus menu (Script > Run script...)
-There will be a dialog asking for the language for the quotes,
-Detected errors shut down the script with an appropriate message.
-TODO : update
-
-"""
 import scribus
 
 non_breaking_space = u"\u00a0"
@@ -37,7 +15,12 @@ non_breaking_thin_space = u"\u202f"
 thin_space = u"\u2009"
 
 def est_espace(text):
-    return (text == ' ') or (text == non_breaking_space) or (text == non_breaking_thin_space)   or (text == thin_space)
+    return (text == ' ') or (text == non_breaking_space) or (text == non_breaking_thin_space) or (text == thin_space)
+
+def ensure_space_before(text):
+    if (lang == 'fr'):
+        return (text == '»') or (text==';') or (text=='!') or (text=='?') or (text==':') or (text=='…')
+    return False
 
 if scribus.haveDoc() <= 0:
     scribus.messageBox('Error - (fr) Erreur', 'You need a Document open\n(fr) Ouvrez un document avant de lancer le script', scribus.ICON_WARNING, scribus.BUTTON_OK)
@@ -48,73 +31,73 @@ if scribus.haveDoc() <= 0:
 #
 lang = scribus.valueDialog("Language", 'Choose language or country\n(fr) Choisissez la langue du texte ou le pays :\naf, be, ch, cs, de, de-g, en, es, et, fi, fr,\n hu, is, lt, mk, nl, pl, ru, se, sk, sl, sq and uk', 'fr')
 if (lang == 'en'):
-    ouvrant_double = u"\u201c" #lead_double
-    fermant_double = u"\u201d" #follow_double
+    lead_double = u"\u201c" #lead_double
+    follow_double = u"\u201d" #follow_double
     lead_single = u"\u2018"
     follow_single = u"\u2019"
 elif (lang == 'de'):
-    ouvrant_double = u"\u201e"
-    fermant_double = u"\u201c"
+    lead_double = u"\u201e"
+    follow_double = u"\u201c"
     lead_single = u"\u2019"
     follow_single = u"\u201a"
 elif (lang == 'de-g'):          # German with inverted guillemets for double quote
-    ouvrant_double = u"\u00bb"
-    fermant_double = u"\u00ab"
+    lead_double = u"\u00bb"
+    follow_double = u"\u00ab"
     lead_single = u"\u2019"
     follow_single = u"\u201a"
 elif (lang == 'fr'):
-    ouvrant_double = u"\u00ab"  #  &laquo; LEFT-POINTING DOUBLE ANGLE QUOTATION MARK
-    fermant_double = u"\u00bb"  #  &raquo; LEFT-POINTING DOUBLE ANGLE QUOTATION MARK
+    lead_double = u"\u00ab"  #  &laquo; LEFT-POINTING DOUBLE ANGLE QUOTATION MARK
+    follow_double = u"\u00bb"  #  &raquo; LEFT-POINTING DOUBLE ANGLE QUOTATION MARK
     lead_single = u"\u2018"     # RIGHT SINGLE QUOTATION MARK
     follow_single = u"\u2019"   # LEFT SINGLE QUOTATION MARK
 elif (lang == 'pl'):
-    ouvrant_double = u"\u201e"
-    fermant_double = u"\u201d"
+    lead_double = u"\u201e"
+    follow_double = u"\u201d"
     lead_single = u"\u201a"
     follow_single = u"\u2019"
 elif ((lang == 'se') or (lang == 'fi')):
-    ouvrant_double = u"\u201d"
-    fermant_double = u"\u201d"
+    lead_double = u"\u201d"
+    follow_double = u"\u201d"
     lead_single = u"\u2019"
     follow_single = u"\u2019"
 elif (lang == 'af'):
-    ouvrant_double = u"\u201c"
-    fermant_double = u"\u201d"
+    lead_double = u"\u201c"
+    follow_double = u"\u201d"
     lead_single = u"\u2018"
     follow_single = u"\u2019"
 elif (lang == 'sq'):
-    ouvrant_double = u"\u201e"
-    fermant_double = u"\u201c"
+    lead_double = u"\u201e"
+    follow_double = u"\u201c"
     lead_single = u"\u2018"
     follow_single = u"\u2019"
 elif ((lang == 'be') or (lang == 'ch') or (lang == 'uk') or (lang == 'ru')):
-    ouvrant_double = u"\u00ab"
-    fermant_double = u"\u00bb"
+    lead_double = u"\u00ab"
+    follow_double = u"\u00bb"
     lead_single = u"\u2039"
     follow_single = u"\u203a"
 elif (lang == 'uk'):
-    ouvrant_double = u"\u00ab"
-    fermant_double = u"\u00bb"
+    lead_double = u"\u00ab"
+    follow_double = u"\u00bb"
     lead_single = u"\u2039"
     follow_single = u"\u203a"
 elif (lang == 'es'):
-    ouvrant_double = u"\u00ab"
-    fermant_double = u"\u00bb"
+    lead_double = u"\u00ab"
+    follow_double = u"\u00bb"
     lead_single = u"\u2018"
     follow_double = u"\u2019"
 elif ((lang == 'lt') or (lang == 'is') or (lang == 'sk') or (lang == 'sl') or (lang == 'cs') or (lang == 'et')):
-    ouvrant_double = u"\u201e"
-    fermant_double = u"\u201c"
+    lead_double = u"\u201e"
+    follow_double = u"\u201c"
     lead_single = u"\u201a"
     follow_single = u"\u2018"
 elif (lang == 'mk'):
-    ouvrant_double = u"\u201e"
-    fermant_double = u"\u201c"
+    lead_double = u"\u201e"
+    follow_double = u"\u201c"
     lead_single = u"\u2019"
     follow_single = u"\u2018"
 elif ((lang == 'hu') or (lang == 'nl')):
-    ouvrant_double = u"\u201e"
-    fermant_double = u"\u201d"
+    lead_double = u"\u201e"
+    follow_double = u"\u201d"
     lead_single = u"\u00bb"
     follow_single = u"\u00ab"
 else:
@@ -143,41 +126,40 @@ if scribus.selectionCount() > 1:
     sys.exit(2)
 
 if (lang =='fr'):
-    typeespace = scribus.valueDialog("Type d'espace",
-                "Selon les polices de caractère utilisées, choisissez le type d'espace ajouté avec les doubles guillemets français, lorsqu'il n'y en a pas déjà un.\n0 : aucun espace ajouté ; 1 : insécable ; 2 : insécable fine ; 3 : fine",
+    space_type = scribus.valueDialog("Type d'espace",
+                "Selon les polices de caractère utilisées, choisissez le type d'espace ajouté avec les doubles guillemets français, lorsqu'il n'y en a pas déjà un.\n0 : aucun espace ajouté ; 1 : insécable fine ; 2 : insécable ; 3 : fine",
                 '1')
 else :
-   typeespace = scribus.valueDialog("Inside quote added space",
-                "Depending on the used fonts, choose the space to be added inside \ndouble quotes, in case there are none already.\n0 : never add a space ; 1 : non breaking ; 2 : non breaking thin ; 3 : thin",
+   space_type = scribus.valueDialog("Inside quote added space",
+                "Depending on the used fonts, choose the space to be added inside \ndouble quotes, in case there are none already.\n0 : dont add a space ; 1 : non breaking thin ; 2 : non breaking ; 3 : thin",
                 '0')
 
-if (typeespace == '3'):
-    spacenquotes = thin_space
-    spacelen = 1
-elif (typeespace == '2'):
-    spacenquotes = non_breaking_thin_space
-    spacelen = 1
-elif (typeespace == '0'):
-    spacenquotes = ''
-    spacelen = 0
+if (space_type == '3'):
+    space_character = thin_space
+    space_len = 1
+elif (space_type == '1'):
+    space_character = non_breaking_thin_space
+    space_len = 1
+elif (space_type == '0'):
+    space_character = ''
+    space_len = 0
 else:
-    spacenquotes = non_breaking_space
-    spacelen = 1
+    space_character = non_breaking_space
+    space_len = 1
 
-if ((1==1) or (spacenquotes != '')):
-    if (lang =='fr'):
-        replace_existing = scribus.valueDialog("Agir sur l'existant ?",
-                "Voulez vous aussi appliquer ce traitement sur les double-guillemets français déjà en place ? Oui: O ; Non : N ",
-                'O')
-    else:
-       replace_existing = scribus.valueDialog("What about existing qyotes ?",
-                "Should the script ALSO apply your spaces-choice on already existing quotes? Yes : Y ; No : N",
-                'N')
-
-if ((replace_existing=='y') or (replace_existing=='Y') or (replace_existing=='o') or (replace_existing=='O')):
-    replace_existing=1
+if (lang =='fr'):
+    replace_existing = scribus.valueDialog("Agir sur l'existant ?",
+            "Voulez vous aussi appliquer ce traitement sur les double-guillemets français déjà en place ? Oui: O ; Non : N ",
+            'O')
 else:
+   replace_existing = scribus.valueDialog("What about existing quotes ?",
+            "Should the script ALSO apply your spaces-choice on already existing quotes? Yes : Y ; No : N",
+            'N')
+
+if (replace_existing == 'n') or (replace_existing == 'N'):
     replace_existing=0
+else:
+    replace_existing=1
 
 textbox = scribus.getSelectedObject()
 boxcount = 1
@@ -197,115 +179,116 @@ for item in scribus.getPageItems():
 textlen = scribus.getTextLength(textbox)
 c = 0
 nbchange = 0
-lastchange = 'close'
+quotes_lastchange = 'close' # la prochaine quote doit être ouvrante
 prevchar = ' '
+is_in_url = False
 
-while c <= (textlen -1):
+while c <= (textlen - 1):
     # si on est à la fin, il faut tricher pour le dernier caractère
     if ((c + 1) > textlen - 1):
-        alafin = 1
+        alafin = True
         nextchar = ' '
     else:
-        alafin = 0
+        alafin = False
         scribus.selectText(c+1, 1, textbox)
         nextchar = scribus.getText(textbox)
        
     scribus.selectText(c, 1, textbox)
     char = scribus.getText(textbox)
+#    scribus.messageBox("Ce qui est", "position:"+str(c) +'\nchar'+str(char)+'\nPrec:'+str(prevchar)+'\nsuiv:'+str(nextchar),
+#          scribus.ICON_WARNING, scribus.BUTTON_OK)
 
-#    scribus.messageBox("ce qui est", "position:"+str(c) +'\nchar'+char+'\nPrec:'+prevchar+'\nsuiv:'+nextchar,
-#           scribus.ICON_WARNING, scribus.BUTTON_OK)
-
-    if (char==ouvrant_double):
-        if (lastchange=='open'):
+    if (char==lead_double):
+        if (quotes_lastchange=='open'):
             if (lang=='fr'):
-                scribus.messageBox("Oups !", 'Incohérence dans les enchainements de guillemets ouvrant et fermant. Une guillement fermante manque avant la position '+str(c) +'\nOn continue quand même',
+                scribus.messageBox("Oups !", 'Incohérence dans les enchainements de guillemets ouvrant et 157. Une guillement fermante manque avant la position '+str(c) +'\nOn continue quand même',
                         scribus.ICON_WARNING, scribus.BUTTON_OK)
             else:
                 scribus.messageBox("Oops !", 'The text is not consistent. Closing doublequote missing before position '+str(c),
                         scribus.ICON_WARNING, scribus.BUTTON_OK)
-        lastchange='open'
-        if ((replace_existing == 1) and (nextchar != spacenquotes) and (alafin==0)):
+        quotes_lastchange='open'
+        if ((replace_existing == 1) and (nextchar != space_character) and (not alafin)):
             if (est_espace(nextchar)):
                 scribus.selectText(c+1, 1, textbox)
                 scribus.deleteText(textbox)
-            scribus.insertText(spacenquotes, c+1, textbox)
+            scribus.insertText(space_character, c+1, textbox)
             nbchange = nbchange+1
 
-    elif (char==fermant_double):
-        if (lastchange=='close'):
+    elif (char==follow_double):
+        if (quotes_lastchange=='close'):
             if (lang=='fr'):
                 scribus.messageBox("Oups !", 'Incohérence dans les enchainements de guillemets ouvrant et fermant. Une guillemet ouvrante manque avant la position '+str(c) +'\nOn continue quand même',
                         scribus.ICON_WARNING, scribus.BUTTON_OK)
             else:
                 scribus.messageBox("Oops !", 'The text is not consistent. Opening doublequote missing before position '+str(c),
                         scribus.ICON_WARNING, scribus.BUTTON_OK)
-        lastchange = 'close'
-        if ((replace_existing == 1)  and (prevchar != spacenquotes) and (c > 1)):
+        quotes_lastchange = 'close'
+        if ((replace_existing == 1)  and (prevchar != space_character) and (c > 1)):
             if (est_espace(prevchar)):
                 scribus.selectText(c-1, 1, textbox)
                 scribus.deleteText(textbox)
                 c=c-1
-            scribus.insertText(spacenquotes, c, textbox)
+            scribus.insertText(space_character, c, textbox)
             nbchange = nbchange+1
-            c=c+spacelen
+            c=c+space_len
    
-    elif (len(char) != 1): #en utf8 certains caractères ont len 2, par ex les espaces spéciaux qu'on teste au dessus
-         rien="rien"          # et ça ferait planter ord()
+    elif (len(char) != 1): # en utf8 certains caractères ont len 2, par ex les espaces spéciaux qu'on teste au dessus
+         rien="rien"       # et ça ferait planter ord()
          
     elif (char == '"'): # autrement dit : ord (char)==34
         #si on trouve une double guillemet droit " en premier caractère du texte, c'est un ouvrant !
         if (c == 0):
             scribus.deleteText(textbox)
             if (not est_espace(nextchar)):
-                scribus.insertText(spacenquotes, 0, textbox)
-            scribus.insertText(ouvrant_double, 0, textbox)
-            lastchange='open'
+                scribus.insertText(space_character, 0, textbox)
+            scribus.insertText(lead_double, 0, textbox)
+            quotes_lastchange='open'
         elif ((prevchar == '.') or (prevchar == ',') or (prevchar == '?') or (prevchar == '!')):
          # lets close after the end of a sentence
             scribus.deleteText(textbox)
-            scribus.insertText(fermant_double, c, textbox)
-            scribus.insertText(spacenquotes, c, textbox)
-            lastchange='close'
-            c=c+spacelen
+            scribus.insertText(follow_double, c, textbox)
+            scribus.insertText(space_character, c, textbox)
+            quotes_lastchange='close'
+            c=c+space_len
         # 39 = ' straight apostrophe
         elif ((ord(prevchar) == 39) and ((nextchar != ' ') and (nextchar != ',') and (nextchar != ';') and (nextchar != '.'))):
             scribus.deleteText(textbox)
             if (not est_espace(nextchar)):
-                scribus.insertText(spacenquotes, c, textbox)
-            scribus.insertText(ouvrant_double, c, textbox)
-            lastchange='open'
+                scribus.insertText(space_character, c, textbox)
+            scribus.insertText(lead_double, c, textbox)
+            quotes_lastchange='open'
         elif ((nextchar == '.') or (nextchar == ',') or (nextchar == ';')):
             scribus.deleteText(textbox)
-            scribus.insertText(fermant_double, c, textbox)
+            scribus.insertText(follow_double, c, textbox)
             if (not est_espace(prevchar)):
-                scribus.insertText(spacenquotes, c, textbox)
-                c=c+spacelen
-            lastchange='close'
-        elif (lastchange!='open'):
+                scribus.insertText(space_character, c, textbox)
+                c=c+space_len
+            quotes_lastchange='close'
+        elif (quotes_lastchange!='open'):
             scribus.deleteText(textbox)
             if (not est_espace(nextchar)):
-                scribus.insertText(spacenquotes, c, textbox)
-            scribus.insertText(ouvrant_double, c, textbox)
-            lastchange='open'
+                scribus.insertText(space_character, c, textbox)
+            scribus.insertText(lead_double, c, textbox)
+            quotes_lastchange='open'
         else:
             scribus.deleteText(textbox)
-            scribus.insertText(fermant_double, c, textbox)
+            scribus.insertText(follow_double, c, textbox)
             if (not est_espace(prevchar)):
-                scribus.insertText(spacenquotes, c, textbox)
-                c=c+spacelen
-            lastchange='close'
+                scribus.insertText(space_character, c, textbox)
+                c=c+space_len
+            quotes_lastchange='close'
         nbchange = nbchange+1
-       
-    elif ((ord(char) == 39) and (c == 0)):
-        scribus.deleteText(textbox)
-        scribus.insertText(lead_single, c, textbox)
-        nbchange = nbchange+1
-    elif (ord(char) == 39):
+
+    elif (ord(char) == 39):                   # typewriter quote
+        if (c==0):                            # au tout début du texte
+            scribus.deleteText(textbox)
+            scribus.insertText(lead_single, c, textbox)
+            nbchange = nbchange+1    
         if ((prevchar == '.') or (prevchar == ',') or (prevchar == '?') or (prevchar == '!')):
             scribus.deleteText(textbox)
             scribus.insertText(follow_single, c, textbox)
         elif ((ord(prevchar) == 34) and ((nextchar != ' ') and (nextchar != ',') and (nextchar != '.'))):
+            # " puis ' puis espace, virgule ou point
             scribus.deleteText(textbox)
             scribus.insertText(lead_single, c, textbox)
         elif ((prevchar != ' ') and (ord(prevchar) != 34) and (nextchar != ' ')):
@@ -319,22 +302,33 @@ while c <= (textlen -1):
             scribus.insertText(follow_single, c, textbox)
         nbchange = nbchange+1
 
+    elif ensure_space_before(char):
+        if (not is_in_url):
+            is_in_url = (char==':') and (prevchar=='p') and (nextchar=='/')
+        
+        if (not est_espace(prevchar)) and (not ensure_space_before(prevchar)) and (not is_in_url):
+            scribus.deleteText(textbox)
+            scribus.insertText(space_character, c, textbox)
+            c += space_len
+            scribus.insertText(char, c, textbox)
+            nbchange = nbchange+1
+
+    elif est_espace(char):
+        is_in_url = False
+
     c += 1
-#   memprevchar = prevchar # pour message de fin seulement
     prevchar = char
     textlen = scribus.getTextLength(textbox)
 
 
 debugmessage = ''
-# debugmessage = '\n\nVérif : les 2 derniers caractères lus étaient <'+str(memprevchar)+'> et <'+str(char)+'>'
-# debugmessage = '\n\nCheck : 2 last analysed characters were <'+str(memprevchar)+'> and <'+str(char)+'>'
 
 scribus.setRedraw(1)
 scribus.docChanged(1)
 
 if (lang == 'fr'):
-    scribus.messageBox("Fini", 'La préparation des quillemets et espaces est faite.\n'+str(nbchange)+' occurrences ont été remplacées' + debugmessage,
-                        icon=scribus.ICON_NONE,button1=scribus.BUTTON_OK)
+    scribus.messageBox("Fini", 'Les corrections typographiques ont été faites.\n'+str(nbchange)+' changements' + debugmessage,
+                        icon=scribus.ICON_NONE, button1=scribus.BUTTON_OK)
 else:
     scribus.messageBox("Done", 'Successfully ran script\n'+str(nbchange)+' replacements have occurred' + debugmessage, # Change this message to your liking
-                        icon=scribus.ICON_NONE,button1=scribus.
+                        icon=scribus.ICON_NONE, button1=scribus.BUTTON_OK)
